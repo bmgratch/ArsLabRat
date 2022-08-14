095d6ca018db94480dc75221c6287abfb28f9aa8
bmgratch <bmgratch@users.noreply.github.com>
2022-08-13 17:01:00 -0700

    create initial labwork.py file

---
+++ labwork.py
@@ -0,0 +1,14 @@
+# labwork.py
+#
+
+import magi
+
+# TODO Lab-work (creating spells or items) consists of two simple parts:
+# The lab total, and the labwork level
+
+labAura = 3
+mags = magi.Magus
+
+# TODO Create the lab total formula
+# The base lab total is Aura + Int + Technique + Form
+
