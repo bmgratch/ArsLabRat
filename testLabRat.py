b28e565078827124dcb462cb63ab316f4901a5bf
bmgratch <bmgratch@users.noreply.github.com>
2022-08-13 17:02:05 -0700

    create testlabrat.py for testing purposes

---
+++ testLabRat.py
@@ -0,0 +1,13 @@
+# test.py
+# Runs tests on the various modules
+
+import magi
+
+testMagus = magi.Magus()
+
+
+print (testMagus.name + ", " + testMagus.house)
+print (testMagus.char)
+print (testMagus.tech, testMagus.form)
+print (testMagus.arts)
+print (testMagus.abilities)
